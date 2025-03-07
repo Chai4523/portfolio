96986053d4aeef795ccd06afa2abbc417aee5562
Chai Chung Yee <chaichungyee@gmail.com>
2025-03-07 12:05:31 +0800

    added new cooklet project under featured project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import vansLife from "../assets/vans_life.jpg";
+import cooklet from "../assets/cooklet.png";
 
 export default function Projects() {
   return (
@@ -19,6 +20,45 @@ export default function Projects() {
           </p>
         </div>
       </div>
+      <a
+        href="https://cooklet.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="card-link"
+      >
+        <div className="img-container">
+          <img
+            src={cooklet}
+            alt="Snapshot of Cooklet application"
+            className="card-img"
+          />
+          <div className="card-info">
+            <h3>Cooklet</h3>
+            <p className="mg-0">
+              A responsive web application that will allow user to search for a
+              variety of food recipe with options to suit their dietary needs.
+              This app is built with
+              <a
+                href="https://spoonacular.com/food-api"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Spoonacular API
+              </a>
+              , hosted on{" "}
+              <a
+                href="https://vercel.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Vercel
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+      </a>
     </div>
   );
 }
